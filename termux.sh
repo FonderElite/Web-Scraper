3d991d20177292503b4ddf2c0b75a28e2c375648
FonderElite <71190959+FonderElite@users.noreply.github.com>
2020-12-26 05:02:57 +0800

    Create termux.sh

---
+++ termux.sh
@@ -0,0 +1,11 @@
+pkg update
+pkg update
+pkg install git -y
+pkg install python
+pkg install python3-pip
+pip3 install -r requirements.txt
+echo "DONE"
+echo "
+▒█▀▀█ █░░█ ▄ 　 ▒█▀▀▀ █▀▀█ █▀▀▄ █▀▀▄ █▀▀ █▀▀█ ▒█▀▀▀ ▒█░░░ ░▀░ ▀▀█▀▀ █▀▀ 
+▒█▀▀▄ █▄▄█ ░ 　 ▒█▀▀▀ █░░█ █░░█ █░░█ █▀▀ █▄▄▀ ▒█▀▀▀ ▒█░░░ ▀█▀ ░░█░░ █▀▀ 
+▒█▄▄█ ▄▄▄█ ▀ 　 ▒█░░░ ▀▀▀▀ ▀░░▀ ▀▀▀░ ▀▀▀ ▀░▀▀ ▒█▄▄▄ ▒█▄▄█ ▀▀▀ ░░▀░░ ▀▀▀"
