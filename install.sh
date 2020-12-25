d71241c43207ccecac1c10b4516952764ce083cc
FonderElite <71190959+FonderElite@users.noreply.github.com>
2020-12-26 03:57:40 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo apt-get update -y
+sudo apt-get upgrade 
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo pip3 install -r requirements.txt
+echo "Current Os Version"
+uname -r 
+echo"DONE!!!"
+echo "
+▒█▀▀█ █░░█ ▄ 　 ▒█▀▀▀ █▀▀█ █▀▀▄ █▀▀▄ █▀▀ █▀▀█ ▒█▀▀▀ ▒█░░░ ░▀░ ▀▀█▀▀ █▀▀ 
+▒█▀▀▄ █▄▄█ ░ 　 ▒█▀▀▀ █░░█ █░░█ █░░█ █▀▀ █▄▄▀ ▒█▀▀▀ ▒█░░░ ▀█▀ ░░█░░ █▀▀ 
+▒█▄▄█ ▄▄▄█ ▀ 　 ▒█░░░ ▀▀▀▀ ▀░░▀ ▀▀▀░ ▀▀▀ ▀░▀▀ ▒█▄▄▄ ▒█▄▄█ ▀▀▀ ░░▀░░ ▀▀▀"
